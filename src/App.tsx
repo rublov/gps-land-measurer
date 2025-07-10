80bbe8fbed26c874401ee9e69c148d438cf8a2a6
[dyad] <git@dyad.sh>
2025-07-10 19:06:04 +0300

    [dyad] Removed redundant toast system for cleaner code - wrote 1 file(s), deleted 3 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -16,7 +15,7 @@ const queryClient = new QueryClient();
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      <Toaster />
+      {/* <Toaster /> - Removed as Sonner is used for all toasts */}
       <Sonner />
       <BrowserRouter>
         <Routes>
