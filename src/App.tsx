def062b3a697f6a32dacaccdab1dbf29f9df5aca
[dyad] <git@dyad.sh>
2025-07-10 20:54:52 +0300

    [dyad] wrote 8 file(s), added @tanstack/react-query, @types/react-router-dom, @types/react-i18next, @types/i18next, @types/date-fns, @types/html2canvas, @types/jspdf, @types/react-day-picker, @types/recharts package(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"; // Добавлен импорт QueryClientProvider
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Index from "./pages/Index";
